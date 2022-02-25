396f6f3ddac38954ea0c9308a0f69ddba569950a
Yang <y.liu@esciencecenter.nl>
2022-02-25 10:57:43 +0100

    fix linting check and add sonar cloud

--- gait_mapper/vae.py
+++ gait_mapper/vae.py
@@ -1,42 +1,50 @@
-import numpy as np
 import tensorflow as tf
 from gait_mapper import utils
+
 
 class VAE(tf.keras.Model):
     """Variational autoencoder of the gait mapper."""
 
-    def __init__(self, window=200, dof=6, latentFeatures=6, alpha=0.1):
+    def __init__(self, window=200, dof=6, latent_features=6, alpha=0.1):
         """
         """
         super(VAE, self).__init__()
         self.window = window
         self.dof = dof
-        self.latentFeatures = latentFeatures
+        self.latent_features = latent_features
         self.alpha = alpha
         self.encoder = self._encoder()
-        #self.encoder.summary() # print summary of encoder model
+        # print summary of encoder model
+        # self.encoder.summary() 
         self.decoder = self._decoder()
-        #self.decoder.summary() # print summary of encoder model
+        # print summary of encoder model
+        # self.decoder.summary()
 
     def _encoder(self):
         """Encoder of the gait mapper.
         """
-        input = tf.keras.layers.Input(shape=(self.window, self.dof))
-        encoder = tf.keras.layers.Conv1D(64, 5,activation='relu',name='conv1')(input)
+        input_layer = tf.keras.layers.Input(shape=(self.window, self.dof))
+        encoder = tf.keras.layers.Conv1D(
+            64, 5, activation='relu', name='conv1')(input_layer)
         encoder = tf.keras.layers.MaxPooling1D(2)(encoder)
-        encoder = tf.keras.layers.Conv1D(64, 3,activation='relu',name='conv2')(encoder)
+        encoder = tf.keras.layers.Conv1D(
+            64, 3, activation='relu', name='conv2')(encoder)
         encoder = tf.keras.layers.MaxPooling1D(2)(encoder)
-        encoder = tf.keras.layers.Conv1D(32, 3, activation='relu',name='conv3')(encoder)
+        encoder = tf.keras.layers.Conv1D(
+            32, 3, activation='relu', name='conv3')(encoder)
         encoder = tf.keras.layers.MaxPooling1D(2)(encoder)
         encoder = tf.keras.layers.Flatten()(encoder)
-        encoder = tf.keras.layers.Dense(16,name='dense1')(encoder)
-        encoder = tf.keras.layers.Dense(8,name='dense2')(encoder)
-        encoder = tf.keras.layers.Dense(8,name='dense3')(encoder)
+        encoder = tf.keras.layers.Dense(16, name='dense1')(encoder)
+        encoder = tf.keras.layers.Dense(8, name='dense2')(encoder)
+        encoder = tf.keras.layers.Dense(8, name='dense3')(encoder)
         encoder = tf.keras.layers.LeakyReLU(self.alpha)(encoder)
         # distribution of latent space variable
-        distribution_mean = tf.keras.layers.Dense(self.latentFeatures, name='mean')(encoder)
-        distribution_variance = tf.keras.layers.Dense(self.latentFeatures, name='log_variance')(encoder)
-        latent_encoding = tf.keras.layers.Lambda(utils.sample_latent_features)([distribution_mean, distribution_variance])
+        distribution_mean = tf.keras.layers.Dense(
+            self.latent_features, name='mean')(encoder)
+        distribution_variance = tf.keras.layers.Dense(
+            self.latent_features, name='log_variance')(encoder)
+        latent_encoding = tf.keras.layers.Lambda(utils.sample_latent_features)(
+            [distribution_mean, distribution_variance])
         encoder_model = tf.keras.Model(input, latent_encoding)
 
         return encoder_model
@@ -44,13 +52,16 @@ class VAE(tf.keras.Model):
     def _decoder(self):
         """Decoder of the gait mapper.
         """
-        input = tf.keras.layers.Input(shape=(self.latentFeatures))
-        decoder = tf.keras.layers.Dense(64)(input)
+        input_layer = tf.keras.layers.Input(shape=(self.latent_features))
+        decoder = tf.keras.layers.Dense(64)(input_layer)
         decoder = tf.keras.layers.Reshape((1, 64))(decoder)
-        decoder = tf.keras.layers.Conv1DTranspose(16, 3, activation='relu')(decoder)
-        decoder = tf.keras.layers.Conv1DTranspose(32, 5, activation='relu')(decoder)
+        decoder = tf.keras.layers.Conv1DTranspose(
+            16, 3, activation='relu')(decoder)
+        decoder = tf.keras.layers.Conv1DTranspose(
+            32, 5, activation='relu')(decoder)
         decoder = tf.keras.layers.UpSampling1D(5)(decoder)
-        decoder = tf.keras.layers.Conv1DTranspose(64, 5, activation='relu')(decoder)
+        decoder = tf.keras.layers.Conv1DTranspose(
+            64, 5, activation='relu')(decoder)
         decoder = tf.keras.layers.UpSampling1D(5)(decoder)
         decoder_output = tf.keras.layers.Conv1DTranspose(6, 6)(decoder)
         decoder_output = tf.keras.layers.LeakyReLU(self.alpha)(decoder_output)
--- tests/test_vae.py
+++ tests/test_vae.py
@@ -2,8 +2,10 @@ from unittest import TestCase
 import gait_mapper
 import gait_mapper.vae
 
+
 class TestGaitMapper():
     """Suite of tests for gait_mapper.vae module."""
+
     def test_encoder_decoder(self):
         """Test if the vae module can generate correct architecture."""
         # define parameters for the model
@@ -11,10 +13,10 @@ class TestGaitMapper():
         degreeOfFreedom = 6
         latentFeatures = 6
         # create encoder and decoder network
-        vae = gait_mapper.vae.VAE(windowLength, degreeOfFreedom, latentFeatures)
+        vae = gait_mapper.vae.VAE(
+            windowLength, degreeOfFreedom, latentFeatures)
         # check if the number of parameters are correct
         expect_num_params_encoder = 32620
         expect_num_params_decoder = 18742
         assert vae.encoder.count_params() == expect_num_params_encoder
         assert vae.decoder.count_params() == expect_num_params_decoder
-
