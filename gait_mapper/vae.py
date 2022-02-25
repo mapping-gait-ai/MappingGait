5965e6c5f193a343098e06d337579787dc4f44cb
Yang <y.liu@esciencecenter.nl>
2022-02-25 15:13:46 +0100

    add docstrings to the network module

--- gait_mapper/vae.py
+++ gait_mapper/vae.py
@@ -5,12 +5,21 @@ from gait_mapper import utils
 class VAE(tf.keras.Model):
     """Variational autoencoder of the gait mapper."""
 
-    def __init__(self, window=200, dof=6, latent_features=6, alpha=0.1):
+    def __init__(self, window_length=200, degrees_of_freedom=6, latent_features=6, alpha=0.1):
         """
+        Initialize the variational autoencoder.
+
+        Args:
+            window_length (int): The unit length of input signal.
+            degrees_of_freedom (int): Degrees of freedom included in the input signal.
+            latent_features (int): Number of latent features to be encoded.
+            alpha (int): Negative slope coefficient of the leaky version of a Rectified Linear Unit.
+        Returns:
+            Encoder and decoder networks.
         """
         super().__init__()
-        self.window = window
-        self.dof = dof
+        self.window_length = window_length
+        self.degrees_of_freedom = degrees_of_freedom
         self.latent_features = latent_features
         self.alpha = alpha
         self.encoder = self._encoder()
@@ -23,7 +32,7 @@ class VAE(tf.keras.Model):
     def _encoder(self):
         """Encoder of the gait mapper.
         """
-        input_layer = tf.keras.layers.Input(shape=(self.window, self.dof))
+        input_layer = tf.keras.layers.Input(shape=(self.window_length, self.degrees_of_freedom))
         encoder = tf.keras.layers.Conv1D(
             64, 5, activation='relu', name='conv1')(input_layer)
         encoder = tf.keras.layers.MaxPooling1D(2)(encoder)
