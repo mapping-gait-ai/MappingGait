06db808c962b30162255af70e4b5c85d816e56cd
Cunliang Geng <c.geng@esciencecenter.nl>
2022-03-01 11:45:36 +0100

    Add tests for training model

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,5 @@
 """This module contains all tests for gait_mapper."""
+from pathlib import Path
+
+test_folder = Path(__file__).resolve().parent
+data_folder = Path(test_folder, 'test_data')
--- tests/test_vae.py
+++ tests/test_vae.py
@@ -1,22 +1,44 @@
-import gait_mapper
-import gait_mapper.vae
+import unittest
+from pathlib import Path
+import numpy as np
+from gait_mapper.vae import VAE
 
+from . import data_folder
 
-class TestGaitMapper():
+class TestGaitMapper(unittest.TestCase):
     """Suite of tests for gait_mapper.vae module."""
+
+    def setUp(self):
+        # define parameters for the model
+        self.window_length = 200
+        # degree_of_freedom
+        self.dof = 6
+        self.latent_features = 6
+        self.data = Path(data_folder,
+            "stored_groupsplit_withoutS01722_latentfeatures_2_frequency_20.npy")
 
     def test_encoder_decoder(self):
         """Test if the vae module can generate correct architecture."""
-        # define parameters for the model
-        window_length = 200
-        # degree_of_freedom
-        dof = 6
-        latent_features = 6
         # create encoder and decoder network
-        vae = gait_mapper.vae.VAE(
-            window_length, dof, latent_features)
+        vae = VAE(
+            self.window_length, self.dof, self.latent_features)
         # check if the number of parameters are correct
         expect_num_params_encoder = 32620
         expect_num_params_decoder = 18742
         assert vae.encoder.count_params() == expect_num_params_encoder
         assert vae.decoder.count_params() == expect_num_params_decoder
+
+
+    def test_training(self):
+        # Prepare data
+        raw_data = np.load(self.data)
+        data = raw_data[:4800].reshape(-1, self.window_length, self.dof)
+
+        # Train model
+        vae = VAE(
+            self.window_length, self.dof, self.latent_features)
+        vae.compile(optimizer="adam")
+        history = vae.fit(data, epochs=3, batch_size=2)
+        self.assertIn('loss', history.history)
+        self.assertIn('reconstruction_loss', history.history)
+        self.assertIn('kl_loss', history.history)
